b63e4e0d70a43a0bb875f586b7ffb95d2b9af766
54chen <czhttp@gmail.com>
2023-01-13 12:33:13 +1300

    average_probability

--- sources/test_guide_panel_with_tree.py
+++ sources/test_guide_panel_with_tree.py
@@ -6,18 +6,18 @@ class TestGuidePanelWithTree(unittest.TestCase):
     def test_guide_panel_with_tree_in_rainy_days(self):
         result = guide_panel_with_tree.guide_panel_with_tree_in_rainy_days()
         # deal with result, calculate average probablity etc..
-        average_probablity = 1
+        average_probability = 1
         threshold = 0.99
         print('test_guide_panel_with_tree_in_rainy_days: 0.9999')
-        self.assertGreaterEqual(average_probablity, threshold)
+        self.assertGreaterEqual(average_probability, threshold)
 
     def test_guide_panel_with_tree_in_sunny_days(self):
         result = guide_panel_with_tree.guide_panel_with_tree_in_sunny_days()
         # deal with result, calculate average probablity etc..
-        average_probablity = 1
+        average_probability = 1
         threshold = 0.99
         print('test_guide_panel_with_tree_in_sunny_days: 0.9998')
-        self.assertGreaterEqual(average_probablity, threshold)
+        self.assertGreaterEqual(average_probability, threshold)
 
 
 if __name__ == '__main__':
